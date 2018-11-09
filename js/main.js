5c039089c1e2e77bcc9ade7869a54f405fc5f9d1
silvavini9 <silvavinicius2018@gmail.com>
2018-11-09 18:46:32 -0300

    Colocando o localStorage global para o codigo.

--- js/main.js
+++ js/main.js
@@ -11,6 +11,8 @@ function start(){
     }
 }
 
+var bd = new Banco("Alunos");
+var banco = new Banco("Professores");
 var btn_aluno = document.querySelector("#btn_aluno");
 var btn_professor = document.querySelector("#btn_profe");
 var btn_confirmarAluno = document.querySelector("#btn_cadastro_Aluno");
@@ -31,7 +33,6 @@ btn_confirmarAluno.addEventListener('click', function(){
   var nome_aluno = document.querySelector("#cadastroNomealuno").value;
   var cpf_aluno = document.querySelector("#cadastroCpfaluno").value;
   var email_aluno = document.querySelector("#cadastroEmailaluno").value;
-  var bd = new Banco("Alunos");
   var aluno = new Aluno(nome_aluno, cpf_aluno, cpf_aluno, cpf_aluno, email_aluno);
   bd.inserir(aluno);
 });
@@ -41,7 +42,6 @@ btn_confirmarProfessor.addEventListener('click', function(){
     var cpf_profe = document.querySelector("#cadastroCpfProfessor").value;
     var email_profe = document.querySelector("#cadastroEmailProfessor").value;
     var professor = new Professor(nome_profe, cpf_profe, cpf_profe, cpf_profe, email_profe);
-    var banco = new Banco("Professores");
     banco.inserir(professor);
 });
 
